28ee53e37d2b3a77d4aa824ce778c726cacff838
Keshao Dhomane <keshaodhomane@gmail.com>
2019-12-01 16:55:59 +0530

    up

--- js/main.js
+++ js/main.js
@@ -26,4 +26,78 @@ $('.slider-for').slick({
     autoplaySpeed: 2000
   });
 
-  
+   
+    
+    
+function switchStyleSheets(theme){
+  $('link[title="theme"]').attr('href', theme);
+  }
+
+$(document).ready(function(){
+    
+    
+    $('#black').click(function(event){
+        event.preventDefault();
+        switchStyleSheets(
+            'css/black.css'
+        );
+    });
+    
+            
+    $('#default').click(function(event){
+        event.preventDefault();
+        switchStyleSheets(
+            'css/green.css'        
+        );
+    });
+    
+    
+    
+    $('#orange').click(function(event){
+        event.preventDefault();
+        switchStyleSheets(
+            'css/orange.css'        
+        );
+    });
+    
+    
+    
+    
+    $('#green').click(function(event){
+        event.preventDefault();
+        switchStyleSheets(
+            'css/default.css'
+        );
+    });
+    
+});
+        
+    
+    
+    /* this changes font size of */
+
+    var originalSize = $('div').css('font-size');
+    // reset
+    $(".resetMe").click(function(){
+    $('div').css('font-size', originalSize); 
+
+    });
+
+    // Increase Font Size
+    $(".increase").click(function(){
+    var currentSize = $('div').css('font-size');
+    var currentSize = parseFloat(currentSize)*1.2;
+    $('div').css('font-size', currentSize);
+
+    return false;
+    });
+
+    // Decrease Font Size
+    $(".decrease").click(function(){
+    var currentFontSize = $('div').css('font-size');
+    var currentSize = $('div').css('font-size');
+    var currentSize = parseFloat(currentSize)*0.8;
+    $('div').css('font-size', currentSize);
+
+    return false;
+    });
